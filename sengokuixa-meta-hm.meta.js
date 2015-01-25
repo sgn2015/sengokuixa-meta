def3eae4e6470dd586e5bd053027244c728dc811
moonlit-g <moonlit1218@gmail.com>
2015-01-25 09:58:59 +0900

    1.4.5.3 自動更新

--- sengokuixa-meta-hm.meta.js
+++ sengokuixa-meta-hm.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           sengokuixa-meta-hm
 // @description    戦国IXAを変態させるツール
-// @version        1.4.5.2
+// @version        1.4.5.3
 // @namespace      sengokuixa-meta
 // @updateURL      https://raw.githubusercontent.com/moonlit-g/sengokuixa-meta/master/sengokuixa-meta-hm.meta.js
 // ==/UserScript==
